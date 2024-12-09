f159cc2e672ef0ad9626408bbe87c71f2aa6814c
Megalotori <159968467+Megalotori@users.noreply.github.com>
2024-12-09 11:50:18 +0000

    add coloured random textures

--- sketch.js
+++ sketch.js
@@ -2,9 +2,25 @@
 // let diameter = 2 * radius;
 const square_side = 50;
 
+// create 4 variables for imgs
+let textureOne;
+let textureTwo;
+let textureThree;
+let textureFour;
+
+let textures;
+
+function preload() {
+  // load each img
+  textureOne = loadImage("Textures/texture-trans1.png");
+  textureTwo = loadImage("Textures/texture-trans2.png");
+  textureThree = loadImage("Textures/texture-trans3.png");
+  textureFour = loadImage("Textures/texture-trans4.png");
+}
+
 function setup() {
   createCanvas(500, 500);
-  background(220);
+  noLoop();
 
   // Circle
 
@@ -16,18 +32,29 @@ function setup() {
   //     circle(x, y, diameter);
   //   }
 
+  // square
+  textures = [textureOne, textureTwo, textureThree, textureFour];
+}
+
+function draw() {
+  background(220);
+
   // Square
 
   // initialising the column, squares co ords are the top left corner
   for (column = 0; column < 10; column = column + 1) {
     // second loop establishes the rows below
-    for (row = 0; row <10; row = row + 1) {
+    for (row = 0; row < 10; row = row + 1) {
       // calculate x & y for each column / row position
       let x = square_side * column;
       let y = square_side * row;
 
+      // sets random textures
+      let randomTexture = random(textures);
+
       // Draws the Square
-      square(x, y, square_side);
+      // square(x, y, square_side);
+      image(randomTexture, x, y, square_side, square_side);
 
       // calculate random colour
       let r = random(0, 255);
@@ -35,9 +62,7 @@ function setup() {
       let b = random(0, 255);
 
       // apply colour "Pick up brush"
-      fill(r, g, b);
+      tint(r, g, b);
     }
   }
 }
-
-function draw() {}
